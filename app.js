17d7893ae741c8829c25a98a176fd5810c4c6141
Alzhan <aismagulov@gmail.com>
2022-09-03 18:22:16 +0600

    05. Создание всех роутов

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const authRoutes = require('./routes/auth') //Регистрируем роуты
+const analyticsRoutes = require('./routes/analytics')
+const categoryRoutes = require('./routes/category')
+const orderRoutes = require('./routes/order')
+const positionRoutes = require('./routes/position')
 const app = express()
 
 app.use('/api/auth', authRoutes) //Показываем, по какому пути будет найден роут
+app.use('/api/analytics', analyticsRoutes)
+app.use('/api/category', categoryRoutes)
+app.use('/api/order', orderRoutes)
+app.use('/api/position', positionRoutes)
 
 module.exports = app
--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,8 @@ const controller = require('../controllers/auth')
 const router = express.Router()
 
 //http://localhost:5000/api/auth/login
-router.get('/login', controller.login)
+router.post('/login', controller.login)
 //http://localhost:5000/api/auth/register
-router.get('/register', controller.register)
+router.post('/register', controller.register)
 
 module.exports = router
