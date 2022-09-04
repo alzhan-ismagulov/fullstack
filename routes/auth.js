13bf2860cd00bd5838fd1e428987cb1bed44e719
Alzhan <aismagulov@gmail.com>
2022-09-05 00:38:30 +0600

    13. Получение всех позиций

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 //http://localhost:5000/api/auth/login
 router.post('/login', controller.login)
+
 //http://localhost:5000/api/auth/register
 router.post('/register', controller.register)
 
