564cb80cef1e1d16c1a576f80cb17e4b9bd4b239
Alzhan <aismagulov@gmail.com>
2022-09-04 14:21:37 +0600

    11. Создание процедуры регистрации и пользователя

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,8 @@
+//Подключаем express
+const { response } = require('express')
+//Подключаем модуль хэширования
+const bcrypt = require('bcryptjs')
+//Подключаем модель User
 const User = require('../models/User')
 
 module.exports.login = function(req, res){
@@ -9,12 +14,40 @@ module.exports.login = function(req, res){
     })
 }
 
-module.exports.register = function(req, res){
-    //email password
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    })
+//Регистрация нового пользователя
 
-    user.save().then(() => console.log('User created'))
+//Экспортируем функцию регистрации
+module.exports.register = async function(req, res){
+    //Поиск пользователя
+   const candidate = await User.findOne({email: req.body.email})
+
+     //Если такой Пользователь уже существует, нужно отправить ошибку
+   if(candidate){
+        res.status(409).json({
+        message: 'Такой емайл уже занят'
+    })
+    //Если такого пользователя нет, то нужно создать пользователя
+   } else {
+    //Добавляем рандомные данные в пароль
+    const salt = bcrypt.genSaltSync(10)
+    //Берём пароль из введённых данных
+    const password = req.body.password
+    //Создаём пользователя
+        const user = new User({
+            //Берём емайл из введённых данных
+            email: req.body.email,
+            //Хэшируем пароль и присваиваем его пользователю
+            password: bcrypt.hashSync(password, salt)
+        })
+
+        try{
+            //Сохраняем пользователя в БД
+            await user.save()  
+            //Показываем, что регистрация прошла успешно, посылая json информацию об емайл и пароле
+            res.status(201).json(user)      
+        } catch(e){
+            //Обработать ошибку
+        }
+        
+   }
 }
